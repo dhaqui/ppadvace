8cfc09b75ae0b49e1cf2635063a3f81c6c2dac70
dhaqui <49834807+dhaqui@users.noreply.github.com>
2025-04-28 16:11:41 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -75,8 +75,8 @@ const createOrder = async (cart) => {
     purchase_units: [
       {
         amount: {
-          currency_code: "USD",
-          value: "10",
+          currency_code: "UGX",
+          value: "100",
         },
       },
     ],
