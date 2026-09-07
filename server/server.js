dd200084ffcc97ec835ce40322e6a1054109afff
dhaqui <49834807+dhaqui@users.noreply.github.com>
2026-09-07 13:33:07 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import fetch from "node-fetch";
 import "dotenv/config";
 
-const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 4000, TEST_COUNTRY = "MW" } = process.env;
+const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 4000, TEST_COUNTRY = "US" } = process.env;
 const base = "https://api-m.sandbox.paypal.com";
 const app = express();
 
