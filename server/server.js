729fa37f7e2526676024c64ca5468cac0d7bbf7f
dhaqui <49834807+dhaqui@users.noreply.github.com>
2026-09-08 13:18:59 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -81,8 +81,8 @@ const SHIPPING_ADDRESS_PRESETS = {
   US_POSTAL_GARBAGE_CITY: {
     address_line_1: "123 Main St",
     admin_area_2: "Zzzznotarealcity999", // 実在しない/郵便番号と整合しない city
-    admin_area_1: "CAA",
-    postal_code: "95131",
+    admin_area_1: "AAA",
+    postal_code: "111111",
     country_code: "US",
   },
 };
